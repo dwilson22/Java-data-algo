ad7abb7b44d96ea1a4935f996b3fcf0b58d7940b
dwilson <dwilson@bluecatnetworks.com>
2019-04-25 11:18:49 -0400

    added insertion sort

--- src/com/danielwilson/InsertionSort.java
+++ src/com/danielwilson/InsertionSort.java
@@ -1,4 +1,39 @@
 package com.danielwilson;
 
 public class InsertionSort {
+    private int[] array;
+
+    InsertionSort(int[] a){
+        array = a;
+    }
+
+    public void sort() {
+        long startTime = System.nanoTime();
+        print();
+
+        for(int firstUnsorted = 1 ; firstUnsorted < array.length; firstUnsorted++){
+            int insertion = array[firstUnsorted];
+
+            int i;
+
+            for(i = firstUnsorted; i > 0 && array[i-1] > insertion; i--){
+                array[i] = array[i-1];
+            }
+
+            array[i] = insertion;
+        }
+
+        print();
+        long endTime = System.nanoTime();
+        System.out.println("InsertionSort took " + (endTime - startTime) + " nanoseconds");
+    }
+
+    private void print(){
+        String p = "";
+        for (int i = 0 ; i < array.length; i ++){
+            p = p + " " + array[i];
+        }
+        System.out.println(p);
+
+    }
 }
--- src/com/danielwilson/Main.java
+++ src/com/danielwilson/Main.java
@@ -5,10 +5,14 @@ public class Main {
     public static void main(String[] args) {
         int[] intBubbleSortArray = {-35, 20 , 45 , 100, 8, -55};
         int[] intSelectionSortArray = {-100, 20 , 45 , 1, 8, -5500};
+        int[] intInsertionSortArray = {-1, 450 , 88 , -856, 200, -5};
+
 
         BubbleSort bubbleSort = new BubbleSort(intBubbleSortArray);
         bubbleSort.sort();
         SelectionSort selectionSort = new SelectionSort(intSelectionSortArray);
         selectionSort.sort();
+        InsertionSort insertionSort = new InsertionSort(intInsertionSortArray);
+        insertionSort.sort();
     }
 }
